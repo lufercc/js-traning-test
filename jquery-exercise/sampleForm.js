7b848a5d592a0e759d79947bf2a2360357e7ec87
luiscachi <lufer_cc@hotmail.com>
2015-07-24 17:26:49 -0400

    lcommited test

--- jquery-exercise/sampleForm.js
+++ jquery-exercise/sampleForm.js
@@ -1,4 +1,11 @@
-var simpeformdemo = function(){
+var SimpleformDemo = function(){
 
+};
+
+SimpleformDemo.prototype.changename= function(){
+    $('input[name="fullname"]').val("luis cachi");
+};
+SimpleformDemo.prototype.email = function(){
+    $('input[name="email"]').val("lufer_cc@hotmail.com");
 };
 
