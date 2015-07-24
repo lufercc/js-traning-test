974e6f4b363de5d2f5b0ac6a244cc5b2fcd7ca6f
luiscachi <lufer_cc@hotmail.com>
2015-07-24 17:57:42 -0400

    mapping page

--- jquery-exercise/sampleForm.js
+++ jquery-exercise/sampleForm.js
@@ -1,11 +1,49 @@
 var SimpleformDemo = function(){
+    this.types = {
 
+        'Press':'press',
+        'Internet':'net',
+        'Word of mouth':?'mouth',
+        'Other': ?'other'
+    };
 };
 
-SimpleformDemo.prototype.changename= function(){
-    $('input[name="fullname"]').val("luis cachi");
+SimpleformDemo.prototype.changename= function(firstname){
+    $('input[name="fullname"]').val(firstname);
 };
-SimpleformDemo.prototype.email = function(){
-    $('input[name="email"]').val("lufer_cc@hotmail.com");
+SimpleformDemo.prototype.email = function(email){
+    $('input[name="email"]').val(email);
 };
+SimpleformDemo.prototype.selectM = function(){
+    $('#genderM').click();
+};
+SimpleformDemo.prototype.selectF = function(){
+    $('#genderF').click();
+};
+SimpleformDemo.prototype.selecSkilling = function() {
+    $('#hobby1').click()
+};
+SimpleformDemo.prototype.selectsrunning = function(){
+    $('#hobby2').click()
+};
+SimpleformDemo.prototype.selectsEating = function(){
+    $('#hobby3').click()
+};
+SimpleformDemo.prototype.selectssleeping = function(){
+    $('#hobby4').click()
+};
+SimpleformDemo.prototype.selectreading = function(){
+    $('#hobby5').click()
+};
+SimpleformDemo.prototype.selectcoding = function() {
+    $('#hobby6').click()
+};
+SimpleformDemo.prototype.selectval= function(val){
+    $('#heard').val(this.types[val]);
+};
+SimpleformDemo.prototype.changeText = function(){
+    
+};
+
+
 
